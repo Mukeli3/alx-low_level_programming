7d26ff2910faa4ef85f8df79cc2ab33a4e0a19ea
Mukeli Kavivya <mukelicynthia20@gmail.com>
2022-10-24 11:29:00 -0700

    Coding space

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest  numbers of 3 integers
+ * largest_number - returns the largest  numbers of three integers
  * @a: first integer
  * @b: second integer
  * @c: third integer
