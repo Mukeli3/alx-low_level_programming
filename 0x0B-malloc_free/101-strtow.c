111e68659f942bcd01a7f1230898ef454c22550b
Mukeli3 <mukelicynthia20@gmail.com>
2022-11-09 16:22:05 -0800

    Coding space

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -30,7 +30,7 @@ char **strtow(char *str)
 	if (str == NULL || *str == '\0')
 		return (NULL);
 	for (c = height = 0; str[c] != '\0'; c++)
-		if (str[c] != '' &&(str[c + 1] == '' || str[c + 1] == '\0'))
+		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			height++;
 	aout = malloc((height + 1) * sizeof(char *));
 	if (aout == NULL || height == 0)
@@ -42,9 +42,9 @@ char **strtow(char *str)
 	{
 		for (c = a1; str[c] != '\0'; c++)
 		{
-			if (str[c] == '')
+			if (str[c] == ' ')
 				a1++;
-			if (str[c] != '' &&(str[c + 1] == '' || str[c + 1] == '\0'))
+			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
 				aout[i] = malloc((c - a1 + 2) * sizeof(char));
 				if (aout[i] == NULL)
