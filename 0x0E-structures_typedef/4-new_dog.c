c459952bc56ad536ea623d872f30e41ecf1d4c07
Mukeli3 <mukelicynthia20@gmail.com>
2022-11-16 16:28:59 -0800

    Coding space

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p_dog;
-	int i, Iname, Iowner;
+
+	int i, lname, lowner;
 
 	p_dog = malloc(sizeof(*p_dog));
 
@@ -22,12 +23,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 		return (NULL);
 	}
-	for (Iname = 0; name[Iname]; Iname++)
+	for (lname = 0; name[lname]; lname++)
 		;
-	for (Iowner = 0; owner[Iowner]; Iowner++)
+	for (lowner = 0; owner[lowner]; lowner++)
 		;
-	p_dog->name = malloc(Iname + 1);
-	p_dog->owner = malloc(Iowner + 1);
+	p_dog->name = malloc(lname + 1);
+	p_dog->owner = malloc(lowner + 1);
 
 	if (!(p_dog->name) || !(p_dog->owner))
 	{
@@ -36,12 +37,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(p_dog);
 		return (NULL);
 	}
-	for (i = 0; i < Iname; i++)
+	for (i = 0; i < lname; i++)
 		p_dog->name[i] = name[i];
 	p_dog->name[i] = '\0';
 	p_dog->age = age;
 
-	for (i = 0; i < Iowner; i++)
+	for (i = 0; i < lowner; i++)
 		p_dog->owner[i] = owner[i];
 	p_dog->owner[i] = '\0';
 	return (p_dog);
