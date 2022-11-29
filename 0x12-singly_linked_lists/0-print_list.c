0166f571b14659526e900cb9258e9b4dca53db4b
Mukeli3 <mukelicynthia20@gmail.com>
2022-11-29 13:49:27 -0800

    Codin space

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (n11)\n");
 		else
-			printf("[d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		nodes++;
 		h = h->next;
