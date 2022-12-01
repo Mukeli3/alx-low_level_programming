e106176dce7de69dece639c724f82fb4881c3d05
Mukeli3 <mukelicynthia20@gmail.com>
2022-12-01 15:01:37 -0800

    Coding space

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * reverse_listint - reverses a listint_t list
- * @head: a pointer to the address of the list_t list
+ * @head: a pointer to the address of the head of the list_t list
  * Return: a pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
