efab56ec8bd429dd9dc94bf02d620d6b43ccc0e9
Mukeli3 <mukelicynthia20@gmail.com>
2022-12-05 15:08:27 -0800

    Coding space

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,7 +4,7 @@
  * set_bit - sets the value of a bit at a given index to 1
  * @n: a pointer to the bit
  * @index: the indexto set the value at - indices start at 0
- * Return: if an error occurs - 1
+ * Return: if an error occurs - -1
  * otherwise - 1
  */
 int set_bit(unsigned long int *n, unsigned int index)
