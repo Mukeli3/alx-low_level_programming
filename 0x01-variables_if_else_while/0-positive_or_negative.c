538bb7c42b9ff19f6cccdbe26eeb30b72db942fb
Mukeli3 <mukelicynthia20@gmail.com>
2023-03-23 10:42:43 +0000

    Positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("n is positive\n");
+		printf("98 is positive\n");
 	}
 	else if (n < 0)
 	{
-		printf("n is negative\n");
+		printf("-98 is negative\n");
 	}
 	else
 	{
-		printf("n is zero\n");
+		printf("0 is zero\n");
 	}
 	return (0);
 }
