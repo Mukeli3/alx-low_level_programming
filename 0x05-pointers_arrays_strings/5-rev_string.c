7d07b9434f99433be314730808ff1dea3a1e24f8
Mukeli3 <mukelicynthia20@gmail.com>
2023-04-13 17:53:28 +0000

    Write a function that reverses a string.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * rev_string - Reverses a string
- * @s: Input string
- * Return: String in reverse
+ * rev_string - reverses a string
+ * @s: input string
+ * Return: string in reverse
  */
 
 void rev_string(char *s)
 {
-	char rev = s[0];
-	int counter = 0;
+	char r = s[0];
+	int c = 0;
 	int i;
 
-	while (s[counter] != '\0')
-	counter++;
-	for (i = 0; i < counter; i++)
+	while (s[c] != '\0')
+	c++;
+	for (i = 0; i < c; i++)
 	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
+		c--;
+		r = s[i];
+		s[i] = s[c];
+		s[c] = r;
 	}
 }
