45f32519e3ef3e668486f813c9e73a7af9e5789e
Mukeli3 <mukelicynthia20@gmail.com>
2023-04-13 15:48:00 +0000

    Write a function that prints a string, in reverse, followed by a new line.

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,10 +14,11 @@ void reverse_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		scanf("%d", &a[i]);
-	_putchar(a[i]);
+	{
+		scanf("%d", &*a);
+		_putchar(*a);
 	for (i = n - 1; i >= 0; i--)
-		_putchar(a[i]);
-
+		_putchar(*a);
+	}
 	_putchar('\n');
 }
