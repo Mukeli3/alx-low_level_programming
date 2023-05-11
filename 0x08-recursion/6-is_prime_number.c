c61b37ee13c2fa4802ef3e18c2006373af256688
Mukeli3 <mukelicynthia20@gmail.com>
2023-05-11 09:56:32 +0000

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,16 +10,12 @@
  */
 int is_prime_number(int n)
 {
-	if (n > 1 && n % n == 0)
+	if (n % (n - 1 / 2) == 0)
 	{
-		return (1);
-	}
-	else if (n % 1 == 0)
-	{
-		return (1);
+		return (0);
 	}
 	else
 	{
-		return (0);
+		return (1);
 	}
 }
