195f7732f60187441f67bacf7b7d21d26fa90811
Mukeli3 <mukelicynthia20@gmail.com>
2023-05-15 14:19:12 +0000

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,13 @@
  * @argc: argument count
  * @argv: argument vector, an array of strings
  *
- * Return: nothing
+ * Return: 0 on success
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
 	printf("%d\n", argc);
+
+	return (0);
 }
