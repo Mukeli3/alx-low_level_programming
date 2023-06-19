eae4eecd8821bc6066e6f83cf91d67b17f0fdbf1
Mukeli3 <mukelicynthia20@gmail.com>
2023-06-19 20:45:46 +0000

    Defining a structure.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,7 @@
-#ifndef _DOG_
-#define _DOG_
-#define struct dog
 
-/**
- * struct dog - a structure type
- * @name: dog name
- * @age: dog age
- * @owner: dog owner
- */
 struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
