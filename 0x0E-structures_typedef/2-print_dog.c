2320de59ef4bb3108459f7748f547676c35da2ca
Mukeli3 <mukelicynthia20@gmail.com>
2023-06-20 19:54:55 +0000

    Print struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,8 +10,33 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	if (d->age == 0)
+	{
+		printf("Age: (nil)");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
 	if (d == NULL)
 	{
-		printf("(nil)");
+		return;
 	}
 }
+
