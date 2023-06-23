ec479ac79fb96ddf858a8ec6e725e90ee3af5871
Mukeli3 <mukelicynthia20@gmail.com>
2023-06-23 14:20:37 +0000

    A function that prints strings, followed by a new line.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -41,14 +41,3 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	printf("\n");
 	va_end(fmm);
 }
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_strings(", ", 2, NULL, "Django");
-    return (0);
-}
