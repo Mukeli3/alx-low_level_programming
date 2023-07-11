aa036663984310fafbb5a3e0e4aeebdd59e3f9ec
Mukeli3 <mukelicynthia20@gmail.com>
2023-07-11 09:30:05 +0000

    Reading and writing a text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,20 +14,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int count, fd;
 	char *buff;
+	ssize_t Re;
 
 	buff = malloc(sizeof(char) * letters);
 	if (!buff)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	Re = read(fd, 
-
-	if (fd < 0)
-		perror("f1");
-		exit(1);
-
-	if (!filename && letters == 0)
-		free(
+	Re = read(fd, buff, letters);
+	if (Re == -1 || fd == -1)
+	{
+		free(buff);
 		return (0);
-	else
-		count = write(fd, filename, letters);
-	return (count)
+	}
+	if (!filename && letters == 0)
+	{
+		free(buff);
+		return (0);
+	}
+	count = write(STDOUT_FILENO, buff, Re);
+	free(buff);
+	close(fd);
+	return (count);
+}
