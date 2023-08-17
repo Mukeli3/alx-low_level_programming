6f300565aed0d1c5a367af4f80e72c01108428c2
Mukeli3 <mukelicynthia20@gmail.com>
2023-08-17 13:45:25 +0000

    print list elements

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function all elements of a list
+ * @h: head, pointer to first node
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t*temp;
+
+	temp = h;
+	if (!h)
+		return (0);
+	while (temp->next)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		count++;
+	}
+	if (!temp->next)
+	{
+		printf("%d\n", temp->n);
+		count++;
+	}
+	return (count);
+}
