428967742aa9d48617e520aa9bfb1de43fd95a0f
Mukeli3 <mukelicynthia20@gmail.com>
2025-05-26 16:00:44 +0000

    fix: single function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * fizz_buzz - prints numbers from 1 to 100 [FizzBuzz]
+ * main - prints numbers from 1 to 100 [FizzBuzz]
  *
  * Return: nothing
  */
-void fizz_buzz(void)
+int main(void)
 {
 	int i = 1;
 
@@ -30,14 +30,5 @@ void fizz_buzz(void)
 		i++;
 	}
 	printf("\n");
-}
-/**
- * main - progran ebtry point
- *
- * Return: nothing
- */
-int main(void)
-{
-	fizz_buzz();
 	return (0);
 }
